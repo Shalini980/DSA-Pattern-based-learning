d8a2ed4d6fa85c4621a97de1028c55b125c03b91
Shalini Chauhan <shalinichauhan965@gmail.com>
2026-07-30 23:37:16 +0530

    sum Tree

---
+++ Trees/DFS-Traversal/sumTree.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int solve(Node* root, bool &ans) {
+        if (root == NULL)
+            return 0;
+
+        if (root->left == NULL && root->right == NULL)
+            return root->data;
+
+        int left = solve(root->left, ans);
+        int right = solve(root->right, ans);
+
+        if (root->data != left + right)
+            ans = false;
+
+        return root->data + left + right;
+    }
+
+    bool isSumTree(Node* root) {
+        bool ans = true;
+        solve(root, ans);
+        return ans;
+    }
+};
