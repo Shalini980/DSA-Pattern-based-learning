983ccdeedc46baf0558fcadd36ba23e7e2ce03ae
Shalini Chauhan <shalinichauhan965@gmail.com>
2026-07-31 14:51:45 +0530

    PathSum II updateed

---
+++ Trees/DFS-Traversal/PathSumII.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+ struct TreeNode {
+     int val;
+    TreeNode *left;
+    TreeNode *right;
+   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+public:
+    void solve(TreeNode* root, int target,
+               vector<int>& path,
+               vector<vector<int>>& ans) {
+
+        if (root == NULL)
+            return;
+
+        path.push_back(root->val);
+        target -= root->val;
+
+        if (root->left == NULL && root->right == NULL) {
+            if (target == 0)
+                ans.push_back(path);
+        }
+
+        solve(root->left, target, path, ans);
+        solve(root->right, target, path, ans);
+
+        path.pop_back();   
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> path;
+
+        solve(root, targetSum, path, ans);
+
+        return ans;
+    }
+};
