1a8f75cb2b4772e19367657a5e27936340830d03
Shalini Chauhan <shalinichauhan965@gmail.com>
2026-08-05 16:14:42 +0530

    maximum length of subarray with equal number of 0 and 1:

---
+++ PrefixSum/EqualNumberOf0and1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        mp[0]=-1;
+        int sum=0;
+        int maxlen=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)nums[i]=-1;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(mp.find(sum)!=mp.end())
+            {
+                maxlen=max(maxlen,i-mp[sum]);
+               
+            }
+            else  mp[sum]=i;
+        }
+        return maxlen;
+    }
+};
