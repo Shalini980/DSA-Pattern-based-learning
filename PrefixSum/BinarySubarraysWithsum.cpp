3fbeffeb6fc39be28a30f7a449f9cc2a6cd11154
Shalini Chauhan <shalinichauhan965@gmail.com>
2026-08-09 15:13:36 +0530

    Binary Subarray with sum

---
+++ PrefixSum/BinarySubarraysWithsum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int count=0;
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(mp.find(sum-goal)!=mp.end())
+            {
+                count+=mp[sum-goal];
+            }
+            mp[sum]++;
+        }
+        return count;
+        
+    }
+};
