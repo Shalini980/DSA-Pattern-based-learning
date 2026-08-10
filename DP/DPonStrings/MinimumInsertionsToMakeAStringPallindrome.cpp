393ea566a2c2040b5cc44329745463aaa36f07e6
Shalini Chauhan <shalinichauhan965@gmail.com>
2026-08-10 17:58:52 +0530

    Minimum Insertions to make a string pallindrome

---
+++ DP/DPonStrings/MinimumInsertionsToMakeAStringPallindrome.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int longestPallindrome(string &s,string &t)
+    {
+        int n=s.length();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=0;
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][n];
+    }
+    int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        int x=longestPallindrome(s,t);
+        int n=s.length();
+        return n-x;
+    }
+};
