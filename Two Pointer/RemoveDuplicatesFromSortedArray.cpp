eb36d5a0784642b8644462dfee03f46035eb4cf8
Shalini Chauhan <shalinichauhan965@gmail.com>
2026-08-16 22:13:47 +0530

    Remove duplicate element from sorted array

---
+++ Two Pointer/RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    vector<int> removeDuplicates(vector<int> &arr) {
+        // code here
+        
+        vector<int>temp;
+        temp.push_back(arr[0]);
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i-1]!=arr[i])
+            {
+                temp.push_back(arr[i]);
+            }
+
+        }
+        return temp;
+    }
+};
